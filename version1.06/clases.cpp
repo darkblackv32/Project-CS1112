8570fcb68d3c451f3dbcec2c6147dd0cf55f08f6
Josroyal <64459744+Josroyal@users.noreply.github.com>
2022-11-22 00:41:31 -0500

    Update clases.cpp
    agregando lo q falta

--- version1.06/clases.cpp
+++ version1.06/clases.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 void menu() {
+  cout << "- - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - - - -" << endl;
+  cout << "BIENVENIDO AL MENU DE BLOCKBUSTER" << endl;
+  cout << "- - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - - - -" << endl;
   cout << "1. Registrar pelicula\n";
   cout << "2. Registrar usuario\n";
   cout << "3. Buscar Pelicula\n";
@@ -10,12 +13,21 @@ void menu() {
   cout << "5. Devolver pelicula\n";
   cout << "6. Reporte pelicula\n";
   cout << "7. Salir\n";
+  cout << "- - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - - - -" << endl;
+  cout << "Ingrese una opcion: " ;  
 }
 
 void menu_buscar_pelicula() {
-  cout << "3.1 Por nombre\n";
-  cout << "3.2 Por anio de publicacion\n";
-  cout << "3.3 Por ranking\n";
+  cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
+  cout <<"Usted se encuentre en el menu de busqueda de peliculas" << endl;
+  cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
+  cout << "1. Por nombre\n";
+  cout << "2. Por anio de publicacion\n";
+  cout << "3. Por ranking\n";
+  cout << "- - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - - - -" << endl;
+  cout << "Ingrese una opcion: "<< endl;
+  cout << "- - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - - - -" << endl;
+  cout << endl;
 }
 
 int main() {
@@ -50,8 +62,11 @@ int main() {
     }
     if (master_key == 6) {
       e1->reporte_peliculas();
+      e1->almacenar_registro();
+
     }
     if (master_key == 7) {
+      e1->cerrar_empresa();
     }
 
   } while (master_key != 7 && master_key > 0 && master_key < 6);
